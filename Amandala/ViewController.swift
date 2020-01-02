5d796829fadb0903b66c2003b91f5822bbecb05c
Denis Markov <keynsianets@gmail.com>
2020-01-02 10:33:34 +0200

    Initial Commit

---
+++ Amandala/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amandala
+//
+//  Created by Денис Марков on 02.01.2020.
+//  Copyright © 2020 Kseniia Shkurenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
